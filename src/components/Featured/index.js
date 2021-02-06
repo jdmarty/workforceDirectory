e885468c930abbacd4aedc00dfa5481f3849f33e
JD Marty <joshua.dmarty@gmail.com>
2021-02-05 19:48:01 -0800

    created featured component

---
+++ src/components/Featured/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Featured() {
+  return (
+    <div className="container mx-auto border-4 border-black">
+      <h1>User Directory!</h1>
+    </div>
+  );
+}
+
+export default Featured;
